5421a38da9d9cd4cd886f5a148b107396eda2b57
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-07-05 05:17:51 +0000

    [Update]配列から重複を削除する

--- lib/split.rb
+++ lib/split.rb
@@ -1,7 +1,7 @@
-a = ",  tama mike　pochi,kuro, shiro"
+a = ",  tama mike　pochi,kuro, shiro tama"
 
 p a.gsub(/[　,]/, " ").strip.split(/[,、 　]+/)
 
-p a.gsub(/[　,、]/, " ").strip.split(/ +/)
+p a.gsub(/[　,、]/, " ").strip.split(/ +/).uniq
 
 
