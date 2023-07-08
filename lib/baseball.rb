c34c27a004d35a376642a124bd5d366387ce71f3
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-07-08 14:17:02 +0000

    [Add]baseball.rb

---
+++ lib/baseball.rb
@@ -0,0 +1,22 @@
+module Baseball
+  class File
+  end
+  
+  class Second
+    def file_with_nesting
+      #入れ子ありのクラス定義でFileクラスを参照する
+      puts File
+    end
+  end
+end
+
+class Baseball::Second
+  def file_without_nesting
+    #入れ子なしのクラス定義でFileクラスを参照する
+    puts File
+  end
+end
+
+second = Baseball::Second.new
+second.file_with_nesting
+second.file_without_nesting
