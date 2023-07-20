fb51ab57ac0303297320810544b263ba31aa034b
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-07-20 13:37:19 +0000

    [Update]count7_7777777.rbのリファクタリング

--- lib/count7_7777777.rb
+++ lib/count7_7777777.rb
@@ -1,12 +1,18 @@
 # 7777777までの数字の7倍数のうち、"7"が何回含まれるか、含まれた回数の合計を出力するプログラム
-def num(create_num)
-  num_list = []
-  for i in 1.upto(create_num) do
-    if(i % 7 == 0)
-      num_list.push(i)
-    end
-  end
-  return num_list.sum { |i| i.to_s.count("7")}
+# def num(create_num)
+#   num_list = []
+#   for i in 1.upto(create_num) do
+#     if(i % 7 == 0)
+#       num_list.push(i)
+#     end
+#   end
+#   return num_list.sum { |i| i.to_s.count("7")}
+# end
+
+# puts num(7777777)
+
+def count7(num)
+  (1..(num / 7)).map { |i| i * 7 }.sum { |n| n.to_s.count('7') }
 end
 
-puts num(7777777)
+puts count7(7777777)
